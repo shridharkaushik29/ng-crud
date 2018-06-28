2a8775c3d9c90c846ae1d86faf01d1882a03bfce
Shridhar Sharma <shridharkaushik29@gmail.com>
2018-06-28 12:59:17 +0530

    Bugs Fixed

--- src/user.js
+++ src/user.js
@@ -2,104 +2,135 @@ import module from "./module";
 
 module
 
-        .factory("$user", [
-            '$rootScope',
-            '$parse',
-            '$crud',
-            '$injector',
-            function ($rootScope, $parse, $crud, $injector) {
+        .provider("$user", function () {
 
-                var rootScopeVariable = "$user";
-                var service = {};
-                var $transitions;
-                if ($injector.has('$transitions')) {
-                    $transitions = $injector.get("$transitions");
+            var defaultConfig = {
+                rootScopeModel: "$user",
+                requests: {
+                    retrieveUser: [
+                        '$crud',
+                        ($crud) => $crud.retrieve("user").then(data => data.user)
+                    ],
+                    login: [
+                        '$crud',
+                        '$params',
+                        ($crud, $params) => $crud.login($params)
+                    ],
+                    logout: [
+                        '$crud',
+                        '$params',
+                        ($crud, $params) => $crud.logout($params)
+                    ],
                 }
+            }
 
-                service.retrieve = function () {
-                    return $crud.retrieve("user").then(function (data) {
-                        var user = data.user;
-                        service.set(user);
-                        return user;
-                    })
-                }
+            this.setRequests = function (requests) {
+                _.merge(defaultConfig.requests, requests);
+            }
 
-                service.get = function () {
-                    return $rootScope.$eval(rootScopeVariable);
-                }
+            this.$get = [
+                '$rootScope',
+                '$parse',
+                '$crud',
+                '$injector',
+                '$q',
+                function ($rootScope, $parse, $crud, $injector, $q) {
 
-                service.set = function (user) {
-                    $parse(rootScopeVariable).assign($rootScope, user);
-                }
+                    var rootScopeVariable = defaultConfig.rootScopeModel;
+                    var service = {};
+                    var $transitions;
+                    if ($injector.has('$transitions')) {
+                        $transitions = $injector.get("$transitions");
+                    }
 
-                service.protectRoutes = function (config) {
-                    var options = _.merge({
-                        defaultState: "profile",
-                        loginState: "login"
-                    }, config);
-
-                    if ($transitions) {
-                        var $stateParams = $injector.get('$stateParams');
-                        $transitions.onEnter({}, function (transition) {
-                            if (transition.to().loggedIn === true || transition.to().loggedOut === true) {
-                                return service.retrieve().then(function (user) {
-                                    if (transition.to().loggedOut === true && user) {
-                                        var to = $stateParams.goto || options.defaultState;
-                                        return transition.router.stateService.target(to, {}, {
-                                            location: 'replace'
-                                        });
-                                    } else if (!user && transition.to().loggedIn === true) {
-                                        return transition.router.stateService.target(options.loginState, {
-                                            goto: transition.to().name
-                                        });
-                                    }
-                                }, function (error) {
-                                    return transition.router.stateService.target(options.loginState, {
-                                        goto: transition.to().name
-                                    });
-                                });
-                            }
+                    service.retrieve = function (params) {
+                        return $injector.invoke(defaultConfig.requests.retrieveUser, this, {
+                            $params: params
+                        }).then((user) => {
+                            service.set(user);
+                            return user;
                         })
                     }
-                }
 
-                service.login = function (params, config) {
-                    var options = {
-                        request: {
-                            reload: true,
-                            requestType: "$post",
-                            checkDataType: true
+                    service.get = () => $rootScope.$eval(rootScopeVariable);
+
+                    service.set = (user) => {
+                        $parse(rootScopeVariable).assign($rootScope, user);
+                        return this;
+                    }
+
+                    service.protectRoutes = function (config) {
+                        var options = _.merge({
+                            defaultState: "profile",
+                            checkLogin: () => $q((resolve, reject) => {
+                                    service.retrieve().then(user => {
+                                        user ? resolve(user) : reject()
+                                    })
+                                }),
+                            loginAction: (transition) => transition.router.stateService.target("login", {
+                                    goto: transition.to().name
+                                })
+                        }, config);
+
+                        if ($transitions) {
+                            var $stateParams = $injector.get('$stateParams');
+                            $transitions.onEnter({}, function (transition) {
+                                if (transition.to().loggedIn === true || transition.to().loggedOut === true) {
+                                    return options.checkLogin(transition).then(function () {
+                                        if (transition.to().loggedOut === true) {
+                                            var to = $stateParams.goto || options.defaultState;
+                                            return transition.router.stateService.target(to, {}, {
+                                                location: 'replace'
+                                            });
+                                        }
+                                    }, function () {
+                                        if (transition.to().loggedIn === true && options.loginAction) {
+                                            return options.loginAction(transition);
+                                        }
+                                    });
+                                }
+                            })
                         }
                     }
 
-                    _.merge(options, config)
-
-                    return $crud.send("login", params, options.request).then(function (data) {
-                        if (data.user !== undefined) {
-                            service.set(data.user);
+                    service.login = function (params, config) {
+                        var options = {
+                            request: {
+                                reload: true,
+                                requestType: "$post",
+                                checkDataType: true
+                            }
                         }
-                        return data;
-                    });
-                }
 
-                service.logout = function (params, config) {
-                    var options = _.merge({
-                        request: {
-                            reload: true,
-                            requestType: "$post",
-                            checkDataType: true
-                        }
-                    }, config)
-                    return $crud.send("logout", params, options.request).then(function (data) {
-                        service.remove();
-                        return data;
-                    });
-                }
+                        _.merge(options, config)
 
-                service.remove = function () {
-                    $parse(rootScopeVariable).assign(undefined, $rootScope);
-                }
+                        return $crud.send("login", params, options.request).then(function (data) {
+                            if (data.user !== undefined) {
+                                service.set(data.user);
+                            }
+                            return data;
+                        });
+                    }
 
-                return service;
-            }
-        ])
+                    service.logout = function (params, config) {
+                        var options = _.merge({
+                            request: {
+                                reload: true,
+                                requestType: "$post",
+                                checkDataType: true
+                            }
+                        }, config)
+                        return $crud.send("logout", params, options.request).then(function (data) {
+                            service.remove();
+                            return data;
+                        });
+                    }
+
+                    service.remove = function () {
+                        $parse(rootScopeVariable).assign(undefined, $rootScope);
+                    }
+
+                    return service;
+                }
+            ]
+        })
